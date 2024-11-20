4a3ab293c0f4c56100e220d03117f76aa3d1939c
“d-jian2233” <2528338161@qq.com>
2024-11-20 21:00:07 +0800

    Finish Account

--- server/common/database.go
+++ server/common/database.go
@@ -13,121 +13,129 @@ var DB *gorm.DB
 // User 用户结构体
 type User struct {
 	gorm.Model
-	UserID       string    `gorm:"column:UserID;primaryKey"`
-	UserName     string    `gorm:"column:UserName"`
-	Email        string    `gorm:"column:Email"`
-	Password     string    `gorm:"column:Password"`
-	RegisterDate time.Time `gorm:"column:RegisterDate"`
-	Surveys      []Survey  `gorm:"foreignKey:UserID"`
+	UserID       string    `gorm:"column:UserID;primaryKey"` // 用户ID
+	UserName     string    `gorm:"column:UserName"`          // 用户名
+	Email        string    `gorm:"column:Email"`             // 邮箱
+	Password     string    `gorm:"column:Password"`          // 密码
+	RegisterDate time.Time `gorm:"column:RegisterDate"`      // 注册日期
+	Surveys      []Survey  `gorm:"foreignKey:UserID"`        // 用户的问卷列表
 }
 
 // Survey 问卷结构体
 type Survey struct {
 	gorm.Model
-	SurveyID          string     `gorm:"column:SurveyID;primaryKey"`
-	AccessID          string     `gorm:"column:AccessID"`
-	UserID            string     `gorm:"column:UserID;index"`
-	Title             string     `gorm:"column:Title"`
-	Description       string     `gorm:"column:Description"`
-	CreateTime        time.Time  `gorm:"column:CreateTime"`
-	ExpireTime        time.Time  `gorm:"column:ExpireTime"`
-	LastUpdateTime    time.Time  `gorm:"column:LastUpdateTime"`
-	LastUpdateUser    string     `gorm:"column:LastUpdateUser"`
-	Status            int        `gorm:"column:Status"`
-	ResponseCount     int        `gorm:"column:ResponseCount"`
-	ThemeColor        int        `gorm:"column:ThemeColor"`
-	TextColor         int        `gorm:"column:TextColor"`
-	PCBackgroundImage string     `gorm:"column:PCBackgroundImage"`
-	PCBannerImage     string     `gorm:"column:PCBannerImage"`
-	Footer            string     `gorm:"column:Footer"`
-	DisplayStyle      int        `gorm:"column:DisplayStyle"`
-	ButtonText        []string   `gorm:"column:ButtonText"`
-	StartTime         time.Time  `gorm:"column:StartTime"`
-	EndTime           time.Time  `gorm:"column:EndTime"`
-	DayStartTime      time.Time  `gorm:"column:DayStartTime"`
-	DayEndTime        time.Time  `gorm:"column:DayEndTime"`
-	PasswordStrategy  int        `gorm:"column:PasswordStrategy"`
-	Password          []string   `gorm:"column:Password"`
-	MaxResponseCount  int        `gorm:"column:MaxResponseCount"`
-	BrowserLimit      bool       `gorm:"column:BrowserLimit"`
-	IPLimit           bool       `gorm:"column:IPLimit"`
-	KeepContent       bool       `gorm:"column:KeepContent"`
-	FailMessage       string     `gorm:"column:FailMessage"`
-	ShowAfterSubmit   int        `gorm:"column:ShowAfterSubmit"`
-	ShowContent       string     `gorm:"column:ShowContent"`
-	Questions         []Question `gorm:"foreignKey:SurveyID"`
-	Responses         []Response `gorm:"foreignKey:SurveyID"`
+	SurveyID          string     `gorm:"column:SurveyID;primaryKey"` // 问卷ID
+	AccessID          string     `gorm:"column:AccessID"`            // 访问ID
+	UserID            string     `gorm:"column:UserID;index"`        // 用户ID
+	Title             string     `gorm:"column:Title"`               // 问卷标题
+	Description       string     `gorm:"column:Description"`         // 问卷描述
+	CreateTime        time.Time  `gorm:"column:CreateTime"`          // 创建时间
+	ExpireTime        time.Time  `gorm:"column:ExpireTime"`          // 过期时间
+	LastUpdateTime    time.Time  `gorm:"column:LastUpdateTime"`      // 最后更新时间
+	LastUpdateUser    string     `gorm:"column:LastUpdateUser"`      // 最后更新用户
+	Status            int        `gorm:"column:Status"`              // 问卷状态
+	ResponseCount     int        `gorm:"column:ResponseCount"`       // 响应数量
+	ThemeColor        int        `gorm:"column:ThemeColor"`          // 主题颜色
+	TextColor         int        `gorm:"column:TextColor"`           // 文字颜色
+	PCBackgroundImage string     `gorm:"column:PCBackgroundImage"`   // PC背景图片
+	PCBannerImage     string     `gorm:"column:PCBannerImage"`       // PC横幅图片
+	Footer            string     `gorm:"column:Footer"`              // 页脚
+	DisplayStyle      int        `gorm:"column:DisplayStyle"`        // 显示样式
+	ButtonText        []string   `gorm:"column:ButtonText"`          // 按钮文本
+	StartTime         time.Time  `gorm:"column:StartTime"`           // 开始时间
+	EndTime           time.Time  `gorm:"column:EndTime"`             // 结束时间
+	DayStartTime      time.Time  `gorm:"column:DayStartTime"`        // 每日开始时间
+	DayEndTime        time.Time  `gorm:"column:DayEndTime"`          // 每日结束时间
+	PasswordStrategy  int        `gorm:"column:PasswordStrategy"`    // 密码策略
+	Password          []string   `gorm:"column:Password"`            // 密码
+	MaxResponseCount  int        `gorm:"column:MaxResponseCount"`    // 最大响应数量
+	BrowserLimit      bool       `gorm:"column:BrowserLimit"`        // 浏览器限制
+	IPLimit           bool       `gorm:"column:IPLimit"`             // IP限制
+	KeepContent       bool       `gorm:"column:KeepContent"`         // 保留内容
+	FailMessage       string     `gorm:"column:FailMessage"`         // 失败消息
+	ShowAfterSubmit   int        `gorm:"column:ShowAfterSubmit"`     // 提交后显示
+	ShowContent       string     `gorm:"column:ShowContent"`         // 显示内容
+	Questions         []Question `gorm:"foreignKey:SurveyID"`        // 问卷中的问题列表
+	Responses         []Response `gorm:"foreignKey:SurveyID"`        // 问卷的响应列表
 }
 
 // Question 问题结构体
 type Question struct {
 	gorm.Model
-	QuestionID       string           `gorm:"column:QuestionID;primaryKey"`
-	SurveyID         string           `gorm:"column:SurveyID;index"`
-	QuestionIndex    int              `gorm:"column:QuestionIndex"`
-	Title            string           `gorm:"column:Title"`
-	Description      string           `gorm:"column:Description"`
-	IsRequired       bool             `gorm:"column:IsRequired"`
-	QuestionType     int              `gorm:"column:QuestionType"`
-	DisplayCondition string           `gorm:"column:DisplayCondition"`
-	Options          []QuestionOption `gorm:"foreignKey:QuestionID"`
+	QuestionID       string           `gorm:"column:QuestionID;primaryKey"` // 问题ID
+	SurveyID         string           `gorm:"column:SurveyID;index"`        // 问卷ID
+	QuestionIndex    int              `gorm:"column:QuestionIndex"`         // 问题索引
+	Title            string           `gorm:"column:Title"`                 // 问题标题
+	Description      string           `gorm:"column:Description"`           // 问题描述
+	IsRequired       bool             `gorm:"column:IsRequired"`            // 是否必填
+	QuestionType     int              `gorm:"column:QuestionType"`          // 问题类型
+	DisplayCondition string           `gorm:"column:DisplayCondition"`      // 显示条件
+	Options          []QuestionOption `gorm:"foreignKey:QuestionID"`        // 问题选项列表
 }
 
 // QuestionOption 问题选项结构体
 type QuestionOption struct {
 	gorm.Model
-	OptionID         string `gorm:"column:OptionID;primaryKey"`
-	QuestionID       string `gorm:"column:QuestionID;index"`
-	SurveyID         string `gorm:"column:SurveyID;index"`
-	OptionIndex      int    `gorm:"column:OptionIndex"`
-	Title            string `gorm:"column:Title"`
-	DisplayCondition string `gorm:"column:DisplayCondition"`
-	Attribute        int    `gorm:"column:Attribute"`
+	OptionID         string `gorm:"column:OptionID;primaryKey"` // 选项ID
+	QuestionID       string `gorm:"column:QuestionID;index"`    // 问题ID
+	SurveyID         string `gorm:"column:SurveyID;index"`      // 问卷ID
+	OptionIndex      int    `gorm:"column:OptionIndex"`         // 选项索引
+	Title            string `gorm:"column:Title"`               // 选项标题
+	DisplayCondition string `gorm:"column:DisplayCondition"`    // 显示条件
+	Attribute        int    `gorm:"column:Attribute"`           // 选项属性
 }
 
 // TextFillIn 文本填空结构体
 type TextFillIn struct {
 	gorm.Model
-	TextFillInID string `gorm:"column:TextFillInID;primaryKey"`
-	QuestionID   string `gorm:"column:QuestionID;index"`
-	TextContent  string `gorm:"column:TextContent"`
+	TextFillInID string `gorm:"column:TextFillInID;primaryKey"` // 文本填空ID
+	QuestionID   string `gorm:"column:QuestionID;index"`        // 问题ID
+	TextContent  string `gorm:"column:TextContent"`             // 文本内容
 }
 
 // NumFillIn 数字填空结构体
 type NumFillIn struct {
 	gorm.Model
-	NumFillInID string `gorm:"column:NumFillInID;primaryKey"`
-	QuestionID  string `gorm:"column:QuestionID;index"`
-	NumContent  int    `gorm:"column:NumContent"`
-	MaxNum      int    `gorm:"column:MaxNum"`
-	LeastNum    int    `gorm:"column:LeastNum"`
-	Precision   int    `gorm:"column:Precision"`
+	NumFillInID string `gorm:"column:NumFillInID;primaryKey"` // 数字填空ID
+	QuestionID  string `gorm:"column:QuestionID;index"`       // 问题ID
+	NumContent  int    `gorm:"column:NumContent"`             // 数字内容
+	MaxNum      int    `gorm:"column:MaxNum"`                 // 最大值
+	LeastNum    int    `gorm:"column:LeastNum"`               // 最小值
+	Precision   int    `gorm:"column:Precision"`              // 精度
 }
 
 // Response 答卷结构体
 type Response struct {
 	gorm.Model
-	ResponseID    string         `gorm:"column:ResponseID;primaryKey"`
-	SurveyID      string         `gorm:"column:SurveyID;index"`
-	ResponseIndex int            `gorm:"column:ResponseIndex"`
-	StartTime     time.Time      `gorm:"column:StartTime"`
-	SubmitTime    time.Time      `gorm:"column:SubmitTime"`
-	Duration      int            `gorm:"column:Duration"`
-	Source        string         `gorm:"column:Source"`
-	IP            string         `gorm:"column:IP"`
-	IsStar        bool           `gorm:"column:IsStar"`
-	IsInvalid     bool           `gorm:"column:IsInvalid"`
-	Data          []ResponseData `gorm:"foreignKey:ResponseID"`
+	ResponseID    string         `gorm:"column:ResponseID;primaryKey"` // 答卷ID
+	SurveyID      string         `gorm:"column:SurveyID;index"`        // 问卷ID
+	ResponseIndex int            `gorm:"column:ResponseIndex"`         // 答卷索引
+	StartTime     time.Time      `gorm:"column:StartTime"`             // 开始时间
+	SubmitTime    time.Time      `gorm:"column:SubmitTime"`            // 提交时间
+	Duration      int            `gorm:"column:Duration"`              // 持续时间
+	Source        string         `gorm:"column:Source"`                // 来源
+	IP            string         `gorm:"column:IP"`                    // IP地址
+	IsStar        bool           `gorm:"column:IsStar"`                // 是否加星
+	IsInvalid     bool           `gorm:"column:IsInvalid"`             // 是否无效
+	Data          []ResponseData `gorm:"foreignKey:ResponseID"`        // 答卷数据列表
 }
 
 // ResponseData 全部答卷内容结构体
 type ResponseData struct {
 	gorm.Model
-	ResponseID string `gorm:"column:ResponseID;index"`
-	SurveyID   string `gorm:"column:SurveyID;index"`
-	QuestionID string `gorm:"column:QuestionID;index"`
-	OptionID   string `gorm:"column:OptionID;index"`
-	Content    string `gorm:"column:Content"`
+	ResponseID string `gorm:"column:ResponseID;index"` // 答卷ID
+	SurveyID   string `gorm:"column:SurveyID;index"`   // 问卷ID
+	QuestionID string `gorm:"column:QuestionID;index"` // 问题ID
+	OptionID   string `gorm:"column:OptionID;index"`   // 选项ID
+	Content    string `gorm:"column:Content"`          // 内容
+}
+
+// EmailVerification 邮箱验证码结构体
+type EmailVerification struct {
+	gorm.Model
+	Email  string    `gorm:"column:Email;index"` // 邮箱
+	Code   string    `gorm:"column:Code"`        // 验证码
+	Expiry time.Time `gorm:"column:Expiry"`      // 过期时间
 }
 
 // InitDb 初始化数据库连接
@@ -143,5 +151,5 @@ func InitDb() {
 	}
 
 	// 自动迁移模式
-	DB.AutoMigrate(&User{}, &Survey{}, &Question{}, &QuestionOption{}, &TextFillIn{}, &NumFillIn{}, &Response{}, &ResponseData{})
+	DB.AutoMigrate(&User{}, &Survey{}, &Question{}, &QuestionOption{}, &TextFillIn{}, &NumFillIn{}, &Response{}, &ResponseData{}, &EmailVerification{})
 }
--- server/services/account_service.go
+++ server/services/account_service.go
@@ -1,1 +1,178 @@
 package services
+
+import (
+	"errors"
+	"math/rand"
+	"net/smtp"
+	"server/common"
+	"time"
+	"github.com/google/uuid"
+	"regexp"
+)
+
+// RegisterUser 注册用户
+func RegisterUser(userName, password, email, emailCode string) error {
+	// 验证用户名格式
+	if len(userName) < 4 || len(userName) > 64 {
+		return errors.New("username must be between 3 and 20 characters")
+	}
+	if !isValidUserName(userName) {
+		return errors.New("username can only contain letters, numbers, and underscores")
+	}
+
+	// 验证密码格式
+	if len(password) < 8 {
+		return errors.New("password must be at least 8 characters long")
+	}
+	if !isValidPassword(password) {
+		return errors.New("password must contain at least one uppercase letter, one lowercase letter, one number, and one special character")
+	}
+
+	// 验证邮箱验证码
+	var emailVerification struct {
+		Email  string
+		Code   string
+		Expiry time.Time
+	}
+	if err := common.DB.Raw("SELECT email, code, expiry FROM email_verifications WHERE email = ? AND code = ?", email, emailCode).Scan(&emailVerification).Error; err != nil {
+		return errors.New("invalid email code")
+	}
+
+	// 检查验证码是否过期
+	if emailVerification.Expiry.Before(time.Now()) {
+		return errors.New("email code expired")
+	}
+
+	// 创建新用户到数据库
+	userID := uuid.New().String()
+	sql := "INSERT INTO users (UserID, UserName, Email, Password, RegisterDate) VALUES (?, ?, ?, ?, ?)"
+	if err := common.DB.Exec(sql, userID, userName, email, password, time.Now()).Error; err != nil {
+		return errors.New("failed to register user")
+	}
+
+	return nil
+}
+
+// SendEmailCode 发送邮箱验证码
+func SendEmailCode(email string) error {
+	// 生成随机验证码
+	code := generateRandomCode(6)
+
+	// 保存验证码到数据库
+	sql := "INSERT INTO email_verifications (Email, Code, Expiry) VALUES (?, ?, ?)"
+	if err := common.DB.Exec(sql, email, code, time.Now().Add(10*time.Minute)).Error; err != nil {
+		return errors.New("failed to save email code")
+	}
+
+	// 发送邮件
+	if err := sendEmail(email, code); err != nil {
+		return errors.New("failed to send email")
+	}
+
+	return nil
+}
+
+// generateRandomCode 生成指定长度的随机数字验证码
+func generateRandomCode(length int) string {
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	code := ""
+	for i := 0; i < length; i++ {
+		code += string(rune('0' + r.Intn(10)))
+	}
+	return code
+}
+
+// sendEmail 发送邮件
+func sendEmail(to, code string) error {
+	from := "your-email@example.com"
+	password := "your-email-password"
+
+	// 设置SMTP服务器信息
+	smtpHost := "smtp.example.com"
+	smtpPort := "587"
+
+	// 设置邮件内容
+	subject := "Subject: Your Verification Code\n"
+	body := "Your verification code is: " + code
+	message := []byte(subject + "\n" + body)
+
+	// 认证信息
+	auth := smtp.PlainAuth("", from, password, smtpHost)
+
+	// 发送邮件
+	err := smtp.SendMail(smtpHost+":"+smtpPort, auth, from, []string{to}, message)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// isValidUserName 验证用户名格式
+func isValidUserName(userName string) bool {
+	// 使用正则表达式验证用户名格式
+	re := regexp.MustCompile(`^[a-zA-Z0-9_]+$`)
+	return re.MatchString(userName)
+}
+
+// isValidPassword 验证密码格式
+func isValidPassword(password string) bool {
+	if len(password) < 8 {
+		return false
+	}
+
+	hasLower := regexp.MustCompile(`[a-z]`).MatchString(password)
+	hasUpper := regexp.MustCompile(`[A-Z]`).MatchString(password)
+	hasDigit := regexp.MustCompile(`\d`).MatchString(password)
+	hasSpecial := regexp.MustCompile(`[@$!%*?&]`).MatchString(password)
+
+	return hasLower && hasUpper && hasDigit && hasSpecial
+}
+
+// LoginUser 用户登录
+func LoginUser(userNameOrEmail, password string) (*common.User, error) {
+	var user common.User
+	// 查询用户信息，用户名或邮箱匹配
+	if err := common.DB.Where("UserName = ? OR Email = ?", userNameOrEmail, userNameOrEmail).First(&user).Error; err != nil {
+		return nil, errors.New("user not found")
+	}
+
+	// 验证密码
+	if user.Password != password {
+		return nil, errors.New("incorrect password")
+	}
+
+	return &user, nil
+}
+
+// GetUserInfoByToken 根据token获取用户信息
+func GetUserInfoByToken(token string) (*common.User, error) {
+	var user common.User
+	//根据token查找用户
+	if err := common.DB.Where("UserID = ?", token).First(&user).Error; err != nil {
+		return nil, errors.New("user not found")
+	}
+	return &user, nil
+}
+
+// ResetPassword 重置用户密码
+func ResetPassword(email, newPassword, emailCode string) error {
+	// 验证邮箱验证码
+	var emailVerification common.EmailVerification
+	if err := common.DB.Where("Email = ? AND Code = ?", email, emailCode).First(&emailVerification).Error; err != nil {
+		return errors.New("invalid email code")
+	}
+
+	// 检查验证码是否过期
+	if emailVerification.Expiry.Before(time.Now()) {
+		return errors.New("email code expired")
+	}
+
+	// 更新用户密码
+	if err := common.DB.Model(&common.User{}).Where("Email = ?", email).Update("Password", newPassword).Error; err != nil {
+		return errors.New("failed to reset password")
+	}
+
+	return nil
+}
+
