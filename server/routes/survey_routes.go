a7d101fc0f0cf392eaf574d820b883b4e26acec7
wch0309 <wongch9@mail2.sysu.edu.cn>
2024-11-21 15:43:25 +0800

    completed the acquisition, replication, and status update of the survey

--- server/routes/survey_routes.go
+++ server/routes/survey_routes.go
@@ -1,1 +1,17 @@
 package routes
+
+import (
+	"server/controllers"
+
+	"github.com/gin-gonic/gin"
+)
+
+func RegisterSurveyRoutes(router *gin.RouterGroup) {
+	surveyGroup := router.Group("/survey")
+	{
+		surveyGroup.POST("/create", controllers.CreateSurvey)       // 问卷创建
+		surveyGroup.POST("/switch", controllers.UpdateSurveyStatus) // 修改问卷状态
+		surveyGroup.POST("/copy", controllers.CopySurvey)           // 问卷复制
+		surveyGroup.GET("", controllers.ListSurveys)                // 获取问卷列表
+	}
+}
